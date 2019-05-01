8fa5959f572e47b0e8840a2226aeafc645a9736f
Agustina Sosa <agustinasf85@gmail.com>
2019-05-01 10:09:07 -0400

    Created CalculatorDisplay component

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import ActionButton from "./components/ButtonComponents/ActionButton"
 import NumberButton from "./components/ButtonComponents/NumberButton"
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay"
 
 const App = () => {
 
