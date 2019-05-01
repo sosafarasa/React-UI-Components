9ec46b871acf6b23386eec2f566aa11eed3d5f15
Agustina Sosa <agustinasf85@gmail.com>
2019-04-30 22:14:17 -0400

    added link to CardContent

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -6,7 +6,7 @@ function CardContent() {
         <div className = "card-content">
             <h2>Get started with React</h2>
             <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-            <h3>reactjs.org</h3>
+            <a href = "https://www.reactjs.org">reactjs.org</a>
         </div>
     );
 };
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -4,12 +4,14 @@ import './Header.css';
 
 import ImageThumbnail from "./ImageThumbnail";
 import HeaderTitle from "./HeaderTitle";
+import HeaderContent from"./HeaderContent"
 
 function HeaderContainer() {
     return (
         <div className = "Header-Container">
             <ImageThumbnail />
             <HeaderTitle />
+            <HeaderContent />
         </div>
     );
 };
