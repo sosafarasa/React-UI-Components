01ef2f041fca3c6348914bea07358765947a7f43
Agustina Sosa <agustinasf85@gmail.com>
2019-04-30 21:32:59 -0400

    Created CardContainer component

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 
 import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
-// import CardContainer from "./components/CardComponents/CardContainer";
+import CardContainer from "./components/CardComponents/CardContainer";
 
 const App = () => {
   return (
     <div className = "App">
       <HeaderContainer />
-      {/* <CardContainer /> */}
+      <CardContainer />
     </div>
   );
 };
